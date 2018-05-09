bffdc168d82217251854096b90f810314819985b
kartikdeshpande <kartikdeshpande18@gmail.com>
2018-05-09 12:22:11 -0400

    Validating fields formats

--- Database.py
+++ Database.py
@@ -7,9 +7,77 @@ class Database:
 	def __init__(self):
                 pass
 
+	# validate for format 1
+	def validate_format(self, field):
+		# Lastname, Firstname, (703)-742-0996, Blue, 10013
+
+		# field_2 should be a phone number		
+		check = field.replace("-","")
+		check = field.replace(" ","")
+		check = field.replace("(","")
+		check = field.replace(")","")
+
+		# should be a 10 digit mobile number
+		if len(check) == 10:
+			return True
+		
+		return False
+
 	def validate(self, input):
 	
-		print input	
+		input = input.split(",")
+		
+		# should contain 5 fields
+		if len(input) <= 4 :
+			return False
+		
+		# 3 formats allowed	
+		# Lastname, Firstname, (703)-742-0996, Blue, 10013
+		# Firstname Lastname, Red, 11237, 703 955 0373
+		# Firstname, Lastname, 10013, 646 111 0101, Green
+			
+		field_1 = input[0]
+		field_2 = input[1]
+		field_3 = input[2]
+		field_4 = input[3]
+		field_5 = input[4]
+
+		entry = {}
+		if self.validate_format(field_2) == True:
+			# validate for format 1, filed_2 should be a mobile number
+
+			entry["color"] =
+			entry["firstname"] =
+			entry["lastname"] =
+                        entry["phonenumber"] =
+                        entry["zipcode"] =
+
+			pass
+		elif self.validate_format(field_5) == True:
+			# validate for format 2, filed_5 should be a mobile number
+
+                        entry["color"] =
+                        entry["firstname"] =
+                        entry["lastname"] =
+                        entry["phonenumber"] =
+                        entry["zipcode"] =
+
+
+			pass
+		elif self.validate_format(field_4) == True:
+			# validate for format 1, filed_4 should be a mobile number
+
+                        entry["color"] =
+                        entry["firstname"] = 
+                        entry["lastname"] = 
+                        entry["phonenumber"] = 
+                        entry["zipcode"] =
+			
+			pass
+		else:
+			# does not match any of the 3 formats
+			return False
+
 
 	def add_to_database(self):
 		pass

634176529a2a60eae967fff2cdaa6157e9068974
kartikdeshpande <kartikdeshpande18@gmail.com>
2018-05-09 11:38:03 -0400

    Basic structure created

---
+++ Database.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+class Database:
+	
+	directory = {}
+	
+	def __init__(self) 
+                pass
+
+	def validate(self, input):
+		pass
+
+	def add_to_database(self):
+		pass
+
+	def validate_and_add(self):
+		pass
+
+if __name__ == '__main__':
+        db = Database()
+        db.validate_and_add()
+
---
+++ FileParser.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+class FileParser:
+
+	filename = ""	
+
+	def __init__(self)
+		pass
+	
+	def read_File(self)
+		pass	
+
+	
+if __name__ == '__main__':
+	reader = FileParser()
+	reader.read_File()		
